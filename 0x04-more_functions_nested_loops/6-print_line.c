40767af02d7e4d9d58f597e1b35ae57ee99034fa
Abelcharles <chuksdgreat1018@gmail.com>
2022-09-15 18:51:28 -0700

    drawing a straigth line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straigth line
+ * @n: number of times _ should be printed
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i 1; i <= n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
